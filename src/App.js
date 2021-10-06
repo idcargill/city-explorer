3932b236d994af128a95c209e065dbbce43c29ae
Ian Cargill <idcargill@gmail.com>
2021-10-05 23:10:29 -0700

    Step 2. api connected and displayed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import InputBox from "./components/InputBox";
 
 function App() {
   return (
-    <div>
-      <h1>React App</h1>
+    <div className='container text-center p-4 m-4'>
+      <InputBox />
     </div>
   );
 }
