59d8bfbb60f3b89a1a5bdb9063f4eccf4d4e31ed
Ian Cargill <idcargill@gmail.com>
2021-10-07 20:28:30 -0700

    component refactor and CSS

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,35 +1,36 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import InputField from "./InputField";
+import LocationDisplay from "../components/LocationDisplay";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class InputBox extends React.Component {
   render() {
     return (
       <>
-        <Container className='bg-success mp-5 text-white search-box'>
-          <h1>City Explorer</h1>
-          <input
-            type='text'
-            placeholder='Enter a city name...'
-            onChange={(e) => this.props.updateCity(e.target.value)}
-          ></input>
-          <Button
-            type='button'
-            className='m-2'
-            variant='warning'
-            onClick={this.props.HandleExplore}
-          >
-            Explore!
-          </Button>
-          {this.props.apiData["display_name"] && (
-            <>
-              <Container className='mb-2 text-display'>
-                <h3>{this.props.apiData["display_name"]}</h3>
-                <h4>Lat: {this.props.apiData.lat}</h4>
-                <h4>Lat: {this.props.apiData.lon}</h4>
-              </Container>
-            </>
-          )}
+        <Container className='mp-5 text-white search-box' fluid={"md"}>
+          <Row>
+            <h1>City Explorer</h1>
+          </Row>
+          <Row>
+            <Col>
+              <InputField
+                onChange={(e) => this.props.updateCity(e.target.value)}
+                updateCity={this.props.updateCity}
+                HandleExplore={this.props.HandleExplore}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              {this.props.apiData["display_name"] && (
+                <>
+                  <LocationDisplay apiData={this.props.apiData} />
+                </>
+              )}
+            </Col>
+          </Row>
         </Container>
       </>
     );
