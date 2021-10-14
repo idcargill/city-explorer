1889acb5cd619c17a36a8f62d38fb1de3afdbe4c
Ian Cargill <idcargill@gmail.com>
2021-10-13 22:11:14 -0700

    w

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import ErrorAlert from "./components/ErrorAlert";
 import InputBox from "./components/InputBox";
 import MapDisplay from "./components/MapDisplay";
-import axios from "axios";
-import Container from "react-bootstrap/Container";
-import getWeatherData from "./helpers/getWeatherData";
 import Weather from "./components/Weather";
+import getWeatherData from "./helpers/getWeatherData";
 import getMovieData from "./helpers/getMovieData";
+import getLocation from "./helpers/getLocation";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import ErrorAlert from "./components/ErrorAlert";
+import MovieDisplay from "./components/MovieDisplay";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -16,7 +19,7 @@ class App extends React.Component {
     this.state = {
       city: "",
       apiData: {},
-      weatherData: {},
+      weatherData: [],
       movies: [],
       error: "",
     };
@@ -31,23 +34,19 @@ class App extends React.Component {
   };
 
   HandleExplore = async () => {
-    const URL = "https://us1.locationiq.com/v1/search.php";
-    const KEY = process.env.REACT_APP_LOCATION_API_KEY;
-    const FORMAT = "json";
-    const fullSearchURL = `${URL}?key=${KEY}&q=${this.state.city}&format=${FORMAT}`;
     try {
-      const apiData = await axios.get(fullSearchURL);
+      const apiData = await getLocation(this.state.city);
       const weatherData = await getWeatherData(apiData, this.state.city);
       const movieData = await getMovieData(apiData, this.state.city);
 
       this.setState({
         apiData: apiData.data[0],
         weatherData: weatherData.data,
-        movieData: movieData,
+        movies: movieData.data,
       });
       this.setState({ error: "" });
     } catch (e) {
-      console.error(e.message);
+      console.error(e);
       this.setState({ error: e });
     }
   };
@@ -55,17 +54,30 @@ class App extends React.Component {
   render() {
     console.log("app state", this.state);
     return (
-      <Container className='text-center main-container'>
-        {this.state.error && <ErrorAlert error={this.state.error} />}
-        <InputBox
-          updateApiData={this.updateApiData}
-          updateCity={this.updateCity}
-          HandleExplore={this.HandleExplore}
-          apiData={this.state.apiData}
-        />
-        {this.state.weatherData[0] && <Weather weatherData={this.state.weatherData} />}
-        {this.state.apiData.lat && <MapDisplay apiData={this.state.apiData} />}
-      </Container>
+      <>
+        <Container className='text-center main-container'>
+          {this.state.error && <ErrorAlert error={this.state.error} />}
+          <InputBox
+            updateApiData={this.updateApiData}
+            updateCity={this.updateCity}
+            HandleExplore={this.HandleExplore}
+            apiData={this.state.apiData}
+          />
+        </Container>
+        <Container>
+          <Row>
+            <Col size='sm' className='col-5' flex>
+              <Container className='movies'>
+                {this.state.movies && <MovieDisplay movies={this.state.movies} />}
+              </Container>
+            </Col>
+            <Col className='col-5'>{this.state.apiData.lat && <MapDisplay apiData={this.state.apiData} />}</Col>
+            <Col className='col-2' flex>
+              {this.state.weatherData.length > 0 && <Weather weatherData={this.state.weatherData} />}
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
--- src/helpers/getLocation.js
+++ src/helpers/getLocation.js
@@ -1,1 +1,13 @@
-const getLocation = (query) => {};
+import axios from "axios";
+require("dotenv").config();
+
+const getLocation = async (city) => {
+  const URL = "https://us1.locationiq.com/v1/search.php";
+  const KEY = process.env.REACT_APP_LOCATION_API_KEY;
+  const FORMAT = "json";
+  const fullSearchURL = `${URL}?key=${KEY}&q=${city}&format=${FORMAT}`;
+  const apiData = await axios.get(fullSearchURL);
+  return apiData;
+};
+
+export default getLocation;
