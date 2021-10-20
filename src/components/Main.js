29d201005ba4df1d2be5ec0056507478bfd051f3
Ian Cargill <idcargill@gmail.com>
2021-10-19 21:58:28 -0700

    wip

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,12 @@
 import React from "react";
-import InputBox from "./components/InputBox";
-import MapDisplay from "./components/MapDisplay";
-import Weather from "./components/Weather";
-import getWeatherData from "./helpers/getWeatherData";
-import getMovieData from "./helpers/getMovieData";
-import getLocation from "./helpers/getLocation";
 import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import ErrorAlert from "./components/ErrorAlert";
-import MovieDisplay from "./components/MovieDisplay";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import InputBox from "./InputBox";
+import ErrorAlert from "./ErrorAlert";
+import getLocation from "../helpers/getLocation";
+import getWeatherData from "../helpers/getWeatherData";
+import getMovieData from "../helpers/getMovieData";
 
-class App extends React.Component {
+class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -24,7 +17,6 @@ class App extends React.Component {
       error: "",
     };
   }
-
   updateApiData = (data) => {
     this.setState({ apiData: data });
   };
@@ -63,22 +55,9 @@ class App extends React.Component {
             apiData={this.state.apiData}
           />
         </Container>
-        <Container>
-          <Row>
-            <Col size='sm' className='col-5' flex>
-              <Container className='movies'>
-                {this.state.movies && <MovieDisplay movies={this.state.movies} />}
-              </Container>
-            </Col>
-            <Col className='col-5'>{this.state.apiData.lat && <MapDisplay apiData={this.state.apiData} />}</Col>
-            <Col className='col-2' flex>
-              {this.state.weatherData.length > 1 && <Weather weatherData={this.state.weatherData} />}
-            </Col>
-          </Row>
-        </Container>
       </>
     );
   }
 }
 
-export default App;
+export default Main;
